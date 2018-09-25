07b9de6cdff9323dc4797795f711c66fd1e1b1aa
wangjingfu <wangjingfu@emicnet.com>
2018-09-25 19:42:14 +0800

    update

--- config/cookie.php
+++
@@ -1,5 +0,0 @@
-<?php 
-return [
-    // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-    'cookieValidationKey' => 'ljOY7X_TB9CUEkLd2HT1g84tqVLjmbg_',
-];
